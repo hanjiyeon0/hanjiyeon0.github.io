93130ffba4aaba8d0db93dbac400928fc2280ef9
hanjiyeon <jiyeon53885@gmail.com>
2021-09-20 16:49:09 +0900

    setting profile image

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+// import PROFILE_IMAGE_LINK from "../../assets/img_profile.png";
+
+const PROFILE_IMAGE_LINK =
+  "https://avatars.githubusercontent.com/u/56794107?s=400&u=e7748331d1141a1d322777558532e7754aaf9538&v=4";
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 20px;
+  border-radius: 50%;
+`;
+const ProfileImage: FunctionComponent = () => {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />;
+};
+
+export default ProfileImage;
